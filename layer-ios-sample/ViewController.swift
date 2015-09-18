d724a0106bb4ac9e2a67a51a6c00190443870379
Shibata Kazuto <shibata.kazuto@dena.jp>
2015-09-18 10:39:55 +0900

    init

---
+++ layer-ios-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  layer-ios-sample
+//
+//  Created by Kazuto Shibata on 2015/09/18.
+//  Copyright (c) 2015年 shibata kazuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
